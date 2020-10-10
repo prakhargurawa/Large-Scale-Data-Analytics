b122dcefcaaa0b71527f095397398d29b5214f03
Prakhar Gurawa <prakhargurawa@gmail.com>
2020-10-11 04:15:44 +0530

    Create UniversalSorter.java

---
+++ Generic_Sorting/UniversalSorter.java
@@ -0,0 +1,49 @@
+public class UniversalSorter <T>{
+    T[] array;
+    java.util.Comparator comparator;
+
+    public void sort(T[] array,java.util.Comparator comparator) { // array length must be a power of 2
+        this.array = array;
+        this.comparator =comparator;
+        sort(0, array.length);
+    }
+
+    public T[] getArray(){
+        return array;
+    }
+
+    private void sort(int low, int n) {
+
+        if (n > 1) {
+            int mid = n >> 1;
+
+            sort(low, mid);
+            sort(low + mid, mid);
+            combine(low, n, 1);
+        }
+    }
+    private void combine(int low, int n, int st) {
+
+        int m = st << 1;
+
+        if (m < n) {
+            combine(low, n, m);
+            combine(low + st, n, m);
+
+            for (int i = low + st; i + st < low + n; i += m)
+                compareAndSwap(i, i + st);
+
+        } else
+            compareAndSwap(low, low + st);
+    }
+
+    private void compareAndSwap(int i, int j) {
+        if (comparator.compare(array[i],array[j])>0)
+            swap(i, j);
+    }
+    private void swap(int i, int j) {
+        T h = array[i];
+        array[i] = array[j];
+        array[j] = h;
+    }
+}
