e63e1be3acbe295d8c6b1921ea892a63a4fb8a3d
Prakhar Gurawa <prakhargurawa@gmail.com>
2020-11-01 07:03:59 +0530

    Create Measurement.java

---
+++ MapReduceJava8Stream/Measurement.java
@@ -0,0 +1,34 @@
+public class Measurement {
+
+    private Integer time;
+    private Double temperature;
+
+    public Measurement(Integer time, Double temperature) {
+        this.time = time;
+        this.temperature = temperature;
+    }
+
+    public Integer getTime() {
+        return time;
+    }
+
+    public void setTime(Integer time) {
+        this.time = time;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "Measurement{" +
+                "time=" + time +
+                ", temperature=" + temperature +
+                '}';
+    }
+}
