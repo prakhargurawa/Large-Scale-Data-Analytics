0f4deef5bb007e468150facec9c9ad17f0f99a72
Prakhar Gurawa <prakhargurawa@gmail.com>
2020-11-01 06:59:44 +0530

    Update SortingPractice.java

--- Generic_Sorting/SortingPractice.java
+++ Generic_Sorting/SortingPractice.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class SortingPractice {
-    public static void main(String args[]){
+    public static void main(String args[]) throws InterruptedException {
         /**********************************
             Question 1 Solution
             -> Approach: Sorter.java
@@ -38,11 +38,14 @@ public class SortingPractice {
          -> Approach 1: UniversalSorter.java
          -> Approach 2: GenericSorter.java
          ***********************************/
-        System.out.println("Question 2-------------------------------------------------------------------------------------");
+        System.out.println("\nQuestion 2-------------------------------------------------------------------------------------");
+        System.out.println("\nApproach 1\n");
+        //Initializing the Universal Sorter class that contains algorithm for sorting generic
         UniversalSorter universalSorter = new UniversalSorter();
         String[] arr2={"prakhar","gurawa","abcd","abba","zasw","zzzz","koko","plpl","yuiop","yuioplk","h","kl","qaz","sdf","qazxs","dawq"};
         System.out.println("Length of array : "+arr2.length);
         if(Util.isPowerOfTwo(arr2.length)) {
+            //The length of array was power of 2
             System.out.println("Content of array before sorting:");
             for (int i = 0; i < arr2.length; i++) {
                 System.out.print(arr2[i] + " ");
@@ -81,6 +84,7 @@ public class SortingPractice {
         }
 
         //Second Approach
+        System.out.println("\nApproach 2\n");
         GenericSorter genericSorter=new GenericSorter();
         Integer[] arr = {12,34,23,45,34,67,68,24,56,11,10,9,8,7,390,456,32,31,30,566,2344,890,900,432,1000,1001,1004,987,900,900,1123,84};
         System.out.println("Length of array : "+arr.length);
@@ -113,18 +117,19 @@ public class SortingPractice {
 
         /**********************************
          Question 3 Solution
-         -> Approach :
          ***********************************/
         System.out.println();
-        System.out.println("Question 3-------------------------------------------------------------------------------------");
+        System.out.println("\nQuestion 3-------------------------------------------------------------------------------------");
         String[] arrL={"prakhar","gurawa","abcd","abba","zasw","zzzz","koko","plpl","yuiop","yuioplk","h","kl","qaz","sdf","qazxs","dawq"};
         System.out.println("Length of array : "+arrL.length);
         if(Util.isPowerOfTwo(arrL.length)) {
+            //The length of array was power of 2
             System.out.println("Content of array before sorting:");
             for (int i = 0; i < arrL.length; i++) {
                 System.out.print(arrL[i] + " ");
             }
             System.out.println();
+            //Providing the comparator as lambda expression
             universalSorter.sort(arrL,(Object o1,Object o2)-> {
                 String c1=(String)o1;
                 String c2=(String)o2;
@@ -144,10 +149,12 @@ public class SortingPractice {
         Double[] arrD={1.2,4.5,6.78,9.08,1.23,7.89,122.56,10101.67,89.90,67.89,12345.67,12.34,12.45,12.567,15.56,9000.98};
         System.out.println("Length of array : "+arrD.length);
         if(Util.isPowerOfTwo(arrD.length)) {
+            //The length of array was power of 2
             System.out.println("Content of array before sorting:");
             for (int i = 0; i < arrD.length; i++) {
                 System.out.print(arrD[i] + " ");
             }
+            //Providing the comparator as lambda expression
             universalSorter.sort(arrD, (Object o1,Object o2)-> {
                 Double c1=(Double)o1;
                 Double c2=(Double)o2;
@@ -173,9 +180,30 @@ public class SortingPractice {
 
         /**********************************
          Question 4 Solution
-         -> Approach :
+         -> Approach : ParallelSorter.java
          ***********************************/
-
+        System.out.println();
+        System.out.println("Question 4-------------------------------------------------------------------------------------");
+        int[] arrP = {12,34,23,45,34,67,68,24,56,11,10,9,8,7,390,456,32,31,30,566,2344,890,900,432,1000,1001,1004,987,900,900,1123,84};
+        if(Util.isPowerOfTwo(arrP.length)) {
+            //The length of array was power of 2
+            System.out.println("Content of array before sorting:");
+            for (int i = 0; i < arrP.length; i++) {
+                System.out.print(arrP[i] + " ");
+            }
+            System.out.println();
+            ParallelSorter parallelSorter=new ParallelSorter();
+            parallelSorter.sort(arrP);
+            int[] arrP2= parallelSorter.getArray();
+            System.out.println("Content of array after sorting:");
+            for (int i = 0; i < arrP2.length; i++) {
+                System.out.print(arrP2[i] + " ");
+            }
+            System.out.println();
+        }else{
+            //The length of array was not power of 2
+            System.out.println("The sorting algorithm is valid for array of length with power of 2");
+        }
     }
 
     static class SortStrings implements java.util.Comparator {
